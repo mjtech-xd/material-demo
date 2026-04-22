8f032dd1f0f6fa1dca58dce944504c9ce64ca05f
manoj.jaiswal <mjaiswal@landolakes.com>
2026-04-22 12:23:52 +0530

    added toolbar and sidenav

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatIconModule, MatCardModule],
+  imports: [MatButtonModule, MatIconModule, MatCardModule, MatToolbarModule, MatSidenavModule, MatDividerModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
